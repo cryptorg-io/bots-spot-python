eb0c5fbadad25ce097f282302880ce8c522f2385
exhum4n <exhum4n@gmail.com>
2018-10-10 09:10:53 +0300

    fixed

---
+++ api.py
@@ -0,0 +1,8 @@
+from Cryptorg import Api
+
+Cryptorg = Api('YOUR_API_KEY', 'YOUR_API_SECRET');
+
+""" First arg must be an array of parrams """
+""" Second arg must be an array of attributes """
+
+print(Cryptorg.status());
